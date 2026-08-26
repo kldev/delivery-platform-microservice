de0971a5252cbf1730a98c98398ee9b16482403e
Krzysztof Łopuch <klopuch@gmail.com>
2026-08-26 20:13:21 +0200

    added lock on row

--- services/delivery-service/src/main/kotlin/com/pl/platform/svc/idempotency/adapter/persistence/IdempotencyJpaRepository.kt
+++ services/delivery-service/src/main/kotlin/com/pl/platform/svc/idempotency/adapter/persistence/IdempotencyJpaRepository.kt
@@ -1,11 +1,20 @@
 package com.pl.platform.svc.idempotency.adapter.persistence
 
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
 import java.util.UUID
 
 interface IdempotencyJpaRepository :
     JpaRepository<IdempotencyJpaEntity, UUID> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("""
+        select idem
+        from IdempotencyJpaEntity idem
+        where idem.idempotencyKey = :key
+    """)
     fun findByIdempotencyKey(
         idempotencyKey: UUID
     ): IdempotencyJpaEntity?
